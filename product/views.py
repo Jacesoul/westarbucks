b55ead82652329011daf5f53623b587a53324edc
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2021-06-15 01:21:43 +0900

    ADD : views.py에 get과 post의 코드 생성

--- product/views.py
+++ product/views.py
@@ -1,5 +1,36 @@
 from django.shortcuts import render
+import json
 
+from django.http import JsonResponse
+from django.views import View
+from product.models import Menu, Category, Product
 # Create your views here.
 
+# class ProductsView(View): 
+#     def post(self, request):
+#         data = json.loads(request.body)
+#         menu = Menu.objects.create(name=data['menu'])
+#         category = Category.objects.create(
+#             name = data['category'],
+#             menu = menu
+#         )
+#         product = Product.objects.create(
+#             name = data['product'],
+#             category = category
+#         )
+#         return JsonResponse({'MESSAGE':'SUCCESS'}, status=201)
 
+
+class ProductsView(View):
+    def get(self, request):
+        products = Product.objects.all()
+        results=[]
+        for product in products:
+            results.append(
+                {
+                    "menu" : product.category.menu.name,
+                    "category" : product.category.name,
+                    "product_name" : product.name
+                }
+            )
+            return JsonResponse({'results':results}, status=200)
